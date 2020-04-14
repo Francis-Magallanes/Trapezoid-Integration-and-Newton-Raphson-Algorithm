432b019e3ad20146a1f5cba9f137ad04b76ba1af
Francis-Magallanes <magallanesfrancisjohn@gmail.com>
2020-04-15 01:20:58 +0800

    It includes comments that will guide the reader in reading and understanding the code

--- src/NewtonRaphson.java
+++ src/NewtonRaphson.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 
 public class NewtonRaphson {
 
-    private static class ColumnData{
+    //this part of the code will create a data structure where it will
+    //store the different computed row values such as the x value,
+    // y value, y' value and relative error
+    private static class RowData {
+
         private double x;
         private double y;
         private double yprime;
@@ -10,58 +14,82 @@ public class NewtonRaphson {
     }
 
     private static double Truncation (double number) {
+
+        //this function will truncate the inputted value
         return Math.floor(number * 100000) / 100000;
     }
 
     private static double Function (double x){
+        //This function is for evaulauting the f(x)
+        //this is where the adjustments are made for different f(x) of the function
+
         return  (4 * Math.pow(x,12)) - (16 * Math.pow(x,11) ) + (3 * Math.pow(x,3)) + ( 64 * x ) + 76;
         //return  Math.pow(x,5) - (16 * Math.pow(x,4) ) + (3 * Math.pow(x,3)) + ( 64 * x ) + 18;
     }
 
     private static double FunctionPrime (double x){
+        //This function is for evaulauting the f'(x)
+        //this is where the adjustments are made for different f'(x) of the function
+
         return  (48 * Math.pow(x,11)) - (176 * Math.pow(x,10) ) + (9 * Math.pow(x,2)) + 64;
      //   return (5 * Math.pow(x,4)) - (64 * Math.pow(x,3) ) + (9 * Math.pow(x,2)) + 64 ;
     }
 
-    private static  ArrayList<ColumnData> ExecuteNewtonRaphson(double initialValueX, double fxStop , double relativeError){
-        ArrayList<ColumnData> table = new ArrayList<>();
+    private static  ArrayList<RowData> ExecuteNewtonRaphson(double initialValueX, double fxStop , double relativeError){
 
-        double computeRelativeError = 10;
+        //this will initialize the dynamic array variable where the row data will be stored and be displayed
+        ArrayList<RowData> table = new ArrayList<>();
+
+        //this will initialize the variables to used for the computation in the loop
+        double computeRelativeError = 10; //this will ensure that the boolean expression in the loop will be true so that the loop will work
         double xvalue = Truncation(initialValueX);
         double yvalue =  Truncation(Function(xvalue));
         double yprime = Truncation(FunctionPrime(xvalue));
 
+        //this loop is for the iterative process of the newton-raphson algorithm
+        //the loop will break if the computed y is below or equal to the y stopping criteria or
+        // if the computed relative error is below or equal to the relative error stopping criteria
         while(yvalue > fxStop && computeRelativeError > relativeError){
 
-            ColumnData columnData = new ColumnData();
-            columnData.x = xvalue;
-            columnData.y = yvalue;
-            columnData.yprime = yprime;
+            //this will initialize the variable where the row data will be stored.
+            RowData rowData = new RowData();
 
-            if(computeRelativeError == 10) columnData.relativeError = 0;
-            else columnData.relativeError = computeRelativeError;
-            table.add(columnData);
+            //this will store the value of x,y and yprime to the variable
+            rowData.x = xvalue;
+            rowData.y = yvalue;
+            rowData.yprime = yprime;
 
+            if(computeRelativeError == 10) rowData.relativeError = 0; //this condition is for storing 0 in the relative error instead of 10 at the start of the iteration
+            else rowData.relativeError = computeRelativeError;
+
+            table.add(rowData);//this will add the row data to dynamic array variable and the data will be stored
+
+            //this part of the code is for the computation of new x value(based from the newton-raphson algorithm), f(x), f'(x), and the relative error
+            //note that the computed values will be truncated for each of the iteration
             xvalue = Truncation(xvalue - (yvalue / yprime));
             yvalue = Truncation( Function(xvalue) );
             yprime = Truncation( FunctionPrime(xvalue) );
-            computeRelativeError = Math.abs(Truncation((xvalue - columnData.x) / xvalue));
+            computeRelativeError = Math.abs(Truncation((xvalue - rowData.x) / xvalue));//note that the rowData.x is the value of the x at the previous iteration
 
         }
 
-        ColumnData columnData = new ColumnData();
-        columnData.x = xvalue;
-        columnData.y = yvalue;
-        columnData.yprime = yprime;
+        //this part of the code will store the row data at which the loop exited
+        //without this, the computed row values which the loop exited will not be displayed
+        RowData rowData = new RowData();
+        rowData.x = xvalue;
+        rowData.y = yvalue;
+        rowData.yprime = yprime;
 
-        if(computeRelativeError == 10) columnData.relativeError = 0;
-        else columnData.relativeError = computeRelativeError;
-        table.add(columnData);
+        if(computeRelativeError == 10) rowData.relativeError = 0;
+        else rowData.relativeError = computeRelativeError;
+        table.add(rowData);
 
-        return table;
+        return table; //returning the computed values
     }
 
-    private static void DisplayTable(ArrayList<ColumnData> table){
+    private static void DisplayTable(ArrayList<RowData> table){
+
+        //this function will display the stored value of the dynamic array through iteration
 
        System.out.println("Results of Newton-Raphson Algorithm: ");
        for (int i = 0 ; i < table.size() ; i++){
@@ -72,7 +100,7 @@ public class NewtonRaphson {
     }
 
     public static void main(String[] args) {
-        DisplayTable( ExecuteNewtonRaphson(7 , 0.001 , 0.0001));
+        DisplayTable( ExecuteNewtonRaphson(7 , 0.001 , 0.0001)); //this will execute the newton-raphson algorithm and it will display the results
     }
 
 }
--- src/TrapezoidIntegration.java
+++ src/TrapezoidIntegration.java
@@ -2,36 +2,51 @@
 public class TrapezoidIntegration {
 
     private static double Truncation (double number) {
+
+        //this function will truncate the inputted value
         return Math.floor(number*100000)/ 100000;
     }
 
     private static double Function(double x){
-        return   (3 * Math.pow(x,3)) + ( 64 * x ) + 76;
+        //This function is for evaulauting the f(x)
+        //this is where the adjustments are made for different f(x) of the integral
+
+         return   (3 * Math.pow(x,3)) + ( 64 * x ) + 76;
         //return (5 * x) - (2 * (Math.pow(x,2))) + (4 * (Math.pow(x,3))) ;
         //return  Math.pow(x,5) - (16 * Math.pow(x,4) ) + (3 * Math.pow(x,3)) + ( 64 * x ) + 18;
 
     }
 
     private static double TrapezoidRule(double lowerLimit, double upperLimit, int numSegments){
-        double sum = 0.0;
 
-        double h = Truncation((upperLimit - lowerLimit) / numSegments);
+        double sum = 0.0; // initialization of the sum
 
-        double yUpperLimit = Truncation( Function(upperLimit) );
+        double h = Truncation((upperLimit - lowerLimit) / numSegments); // this is for the computation and truncation of h
 
-        double yLowerLimit = Truncation( Function(lowerLimit) );
+        double yUpperLimit = Truncation( Function(upperLimit) ); //evaluating and truncating the f(b)
 
+        double yLowerLimit = Truncation( Function(lowerLimit) ); // evaluating and truncating the f(a)
+
+        // this is the part where the sum of f(a + ih) is computed
+        //note that the value of f(a+ih) is truncated first before adding to the sum
         for(int i = 1 ; i <= numSegments - 1 ; i++ ) sum += Truncation( Function( lowerLimit + (i * h) ) );
 
         sum = 2 * sum ;
 
-        double multiplier = Truncation((upperLimit - lowerLimit) / (2 * numSegments) ) ;
+        double multiplier = Truncation((upperLimit - lowerLimit) / (2 * numSegments) ) ; //computating the truncated value of (b-a)/2n
 
+        // applying the formula the based from the computed values
+        //the result of this computation will be truncated too.
         return Truncation( multiplier * (yUpperLimit + sum + yLowerLimit)) ;
+
     }
 
     public static void main(String[] args) {
-        System.out.println("The answer is " + TrapezoidRule(10, 20,32));
+        //"TrapezoidRule(10, 20,64)" will execute the trapezoid rule algorithm based on the hard-coded f(x)
+        // 10 is the lower limit of integration parameter while 20 is the upper limit of integration parameter
+        // 64 is the number of segments parameter
+        // these parameters need to be adjusted manually depending on the definite integral to be computed
+        System.out.println("The answer is " + TrapezoidRule(10, 20,64));
     }
 
 }
